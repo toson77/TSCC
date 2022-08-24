8279a7496e76cab9cd9150be9cbb58fc2ee8d41d
Noriyuki <norifxsc@gmail.com>
2022-08-24 21:11:07 +0900

    step5

--- tscc.c
+++ tscc.c
@@ -93,7 +93,7 @@ Token *tokennize() {
 			p++;
 			continue;
 		}
-		if(*p == '+' || *p == '-') {
+		if(strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -104,7 +104,7 @@ Token *tokennize() {
 			continue;
 		}
 
-		error_at(p, "トークナイズできません");
+		error_at(p, "invalid token");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -112,6 +112,118 @@ Token *tokennize() {
 }
 
 
+//parser
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+	NodeKind kind; // Node kind
+	Node *lhs; 		// Left-hand side
+	Node *rhs; 		// Right-hand side
+	int val; 		// Used if kind == ND_NUM
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *mul();
+Node *primary();
+
+// expr = mul('+' mul | "-" mul)*
+Node *expr() {
+	Node *node = mul();
+
+	for(;;) {
+		if (consume('+')) {
+			node = new_node(ND_ADD, node, mul());
+		}
+		else if (consume('-')) {
+			node = new_node(ND_SUB, node, mul());
+		}
+		else {
+			return node;
+		}
+	}
+}
+
+// mul = primary("*" primary | "/" primary)*
+Node *mul() {
+	Node *node = primary();
+	for(;;) {
+		if (consume('*')) {
+			node = new_node(ND_MUL, node, primary());
+		}
+		else if (consume('/')) {
+			node = new_node(ND_DIV, node, primary());
+		}
+		else {
+			return node;
+		}
+	}
+}
+
+// primary = "(" expr ")" | num
+
+Node *primary() {
+	if(consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+// code gen(stack machine)
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+		case ND_ADD:
+			printf("  add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("  sub rax, rdi\n");
+			break;
+		case ND_MUL:
+			printf(" imul rax, rdi\n");
+			break;
+		case ND_DIV:
+			printf("  cqo\n");
+			printf("  idiv rdi\n");
+			break;
+	}
+
+	printf("  push rax\n");
+}
 
 
 int main(int argc, char **argv) {
@@ -122,25 +234,17 @@ int main(int argc, char **argv) {
 
 	user_input = argv[1];
 	token = tokennize();
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf(" 	mov rax, %d\n", expect_number());
 
-	while(!at_eof()) {
-		if(consume('+')) {
-			printf(" 	add rax, %d\n", expect_number());
-			continue;
-		}
+	gen(node);
 
-		if (consume('-')) {
-			printf(" 	sub rax, %d\n", expect_number());
-			continue;
-		}
-
-	}
-
+	// return  value in stack top
+	printf("  pop rax\n");
+	// return from function
 	printf(" 	ret\n");
 	return 0;
 }
